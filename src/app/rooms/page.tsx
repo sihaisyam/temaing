1f3b464fef3b465926f628ad08a2f0e41be2b83e
unknown <zahrrranabill11@gmail.com>
2025-05-28 17:13:14 +0700

    Pertemuan 13

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -19,11 +19,12 @@ const RoomsPage = () => {
   const [message, setMessage] = useState("");
   const [roomName, setRoomName] = useState("");
   const [name, setName] = useState("");
+  const [roomId, setRoomId] = useState(0);
   const [categoryId, setCategoryId] = useState(0);
   const [price, setPrice] = useState(0);
   const [capacity, setCapacity] = useState(0);
   const [desc, setDesc] = useState("");
-  
+
   const accessToken = localStorage.getItem("accessToken");
 
   const handleGet = async () => {
@@ -49,14 +50,13 @@ const RoomsPage = () => {
 
   const handleSubmit = async () => {
     try {
-
       const payload = {
-        "name": name,
-        "categoryId": categoryId,
-        "price": price,
-        "capacity": capacity,
-        "description": desc
-      }
+        name: name,
+        categoryId: categoryId,
+        price: price,
+        capacity: capacity,
+        description: desc,
+      };
 
       const response = await fetch("https://simaru.amisbudi.cloud/api/rooms", {
         method: "POST",
@@ -64,23 +64,58 @@ const RoomsPage = () => {
           "Content-Type": "application/json",
           Authorization: "Bearer " + accessToken,
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(payload),
       });
 
       const { data, message } = await response.json();
       console.log(data);
       if (data) {
-        setMessage(message)
-        setRoomName(data.name)
-        setISuccess(true)
-        setIsOpen(false)
-        setTimeout(() => setISuccess(false), 3000)
-        handleGet()
+        setMessage(message);
+        setRoomName(data.name);
+        setISuccess(true);
+        setIsOpen(false);
+        setTimeout(() => setISuccess(false), 3000);
+        handleGet();
       }
     } catch (err) {
       // setError('An error occurred. Please try again.');
     } finally {
       // setIsLoading(false);
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const payload = {
+        name: name,
+        categoryId: categoryId,
+        price: price,
+        capacity: capacity,
+        description: desc,
+      };
+
+      const response = await fetch(`https://simaru.amisbudi.cloud/api/rooms/${roomId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + accessToken,
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const { data, message } = await response.json();
+      console.log(data);
+      if (data) {
+        setMessage(message);
+        setRoomName(data.name);
+      }
+    } catch (err) {
+      // setError('An error occurred. Please try again.');
+    } finally {
+        setISuccess(true);
+        setIsEdit(false);
+        setTimeout(() => setISuccess(false), 3000);
+        handleGet();
     }
   };
 
@@ -91,9 +126,24 @@ const RoomsPage = () => {
   // const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(search.toLowerCase()));
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
   const [isSuccess, setISuccess] = useState(false);
   const actionModal = () => {
     setIsOpen(!isOpen);
+  };
+
+  const actionModalEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const actionEdit = (room: Room) => {
+    console.log(room)
+    setRoomId(room.id)
+    setIsEdit(true)
+    setName(room.name)
+    setCategoryId(room.categoryId)
+    setCapacity(room.capacity)
+    setDesc(room.description)
   };
 
   return (
@@ -125,7 +175,7 @@ const RoomsPage = () => {
                       <label htmlFor="category" className="block text-sm font-medium text-gray-700">
                         Category
                       </label>
-                      <input type="text" onChange={(e) => setCategoryId(+e.target.value)}  id="category" name="category" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                      <input type="text" onChange={(e) => setCategoryId(+e.target.value)} id="category" name="category" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
                     </div>
                   </div>
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -148,7 +198,7 @@ const RoomsPage = () => {
                       <label htmlFor="description" className="block text-sm font-medium text-gray-700">
                         Description
                       </label>
-                      <input type="text" onChange={(e) => setDesc(e.target.value)}  id="description" name="description" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                      <input type="text" onChange={(e) => setDesc(e.target.value)} id="description" name="description" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
                     </div>
                   </div>
                 </div>
@@ -156,7 +206,77 @@ const RoomsPage = () => {
                   <button onClick={actionModal} className="py-2.5 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                     Kembali
                   </button>
-                  <button className="text-white ms-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit} type="button">Simpan</button>
+                  <button className="text-white ms-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit} type="button">
+                    Simpan
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isEdit && (
+        <div id="default-modal" className="fixed top-0 left-0 right-0 z-50 flex justify-center items-center w-full h-full bg-gray-900 bg-opacity-50">
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Edit Data</h3>
+                <button type="button" onClick={actionModalEdit} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <form className="space-y-4">
+                <div className="p-4 md:p-5 space-y-4">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                        Name
+                      </label>
+                      <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" name="name" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+
+                    <div>
+                      <label htmlFor="category" className="block text-sm font-medium text-gray-700">
+                        Category
+                      </label>
+                      <input type="text" value={categoryId} onChange={(e) => setCategoryId(+e.target.value)} id="category" name="category" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                      <label htmlFor="price" className="block text-sm font-medium text-gray-700">
+                        Price
+                      </label>
+                      <input type="text" value={price} onChange={(e) => setPrice(+e.target.value)} id="price" name="price" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+
+                    <div>
+                      <label htmlFor="capacity" className="block text-sm font-medium text-gray-700">
+                        Capacity
+                      </label>
+                      <input type="text" value={capacity} onChange={(e) => setCapacity(+e.target.value)} id="capacity" name="capacity" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                  </div>
+                  <div className="grid gap-4">
+                    <div>
+                      <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                        Description
+                      </label>
+                      <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)} id="description" name="description" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <button onClick={actionModalEdit} className="py-2.5 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    Kembali
+                  </button>
+                  <button className="text-white ms-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleUpdate} type="button">
+                    Simpan
+                  </button>
                 </div>
               </form>
             </div>
@@ -212,6 +332,7 @@ const RoomsPage = () => {
                   <th className="py-2 px-4 border">Name</th>
                   <th className="py-2 px-4 border">Capacity</th>
                   <th className="py-2 px-4 border">Status</th>
+                  <th className="py-2 px-4 border">Action</th>
                 </tr>
               </thead>
               <tbody>
@@ -221,6 +342,14 @@ const RoomsPage = () => {
                     <td className="py-2 px-4 border">{room.name}</td>
                     <td className="py-2 px-4 border">{room.capacity}</td>
                     <td className="py-2 px-4 border">{room.status}</td>
+                    <td className="flex py-2 px-4 border justify-center gap-2">
+                      <button onClick={() => actionEdit(room) } className="bg-yellow-900 hover:bg-yellow-600 text-white px-4 py-2 rounded-md">
+                        Edit
+                      </button>
+                      <button className="bg-red-600 hover:bg-red-200 text-white px-4 py-2 rounded-md">
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 ))}
                 {/* {filteredUsers.map((user) => (
